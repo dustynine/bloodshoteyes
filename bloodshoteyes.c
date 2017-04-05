2efcb9ac664d8c34d2720074cdeae79ca9d05595
Dusty Nine <27400@protonmail.ch>
2017-04-05 21:55:17 +0600

    rudimentary functionality

--- bloodshoteyes.c
+++ bloodshoteyes.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
 
 void main(int argc, char *argv[]) {
-    
+
     if (argc !=3) { // finish input filtering part
         printf("Usage: %s <mins to work> <mins to rest>\n", argv[0]);
         exit(0);
@@ -13,7 +14,17 @@ void main(int argc, char *argv[]) {
     worktime = atoi(argv[1]);
     breaktime = atoi(argv[2]);
 
-    time_t time_till = clock();
-    printf("%ld", (long) time_till);
-    // usleep() ???
+    time_t rawtime;
+    struct tm * timeinfo;
+
+    time(&rawtime);
+    timeinfo = localtime(&rawtime);
+    printf("Starting at %s\n", asctime(timeinfo));
+
+    while(1) {
+        sleep(60*worktime);
+        printf("Go get some rest now.\n");
+        sleep(60*breaktime);
+        printf("Ok, now back to work.\n");
+    }
 }
