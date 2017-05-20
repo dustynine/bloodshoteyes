bb4e81281e8b8a173605c318416315d5a4ab9782
Dusty Nine <27400@protonmail.ch>
2017-05-20 12:25:05 +0600

    0.1

--- bse.py
+++ bse.py
@@ -24,80 +24,81 @@ for opt, arg in opts:
     elif opt in ("-r","--rest"):
         resttime = float(arg)
 
+# set a fraction by which progress bar will progress
+# according to rest time
 fraction = 0.05 / (resttime * 60)
-# set a fraction according to rest time
 
 class ReminderPopup(Gtk.Window):
 
     def __init__(self):
-        Gtk.Window.__init__(self, title="Take a break now.")
-        self.set_border_width(10)
+        Gtk.Window.__init__(self, \
+                title           = "Take a break now.", \
+                border_width    = 10, \
+                #default_width   = 250, \
+                #default_height  = 70, \
+                decorated       = False, \
+                deletable       = False, \
+                resizable       = False) 
+        self.set_keep_above(True)
+        self.stick()
 
-        vbox = Gtk.Box(orientation=Gtk.Orientation.VERTICAL, spacing=6)
-        self.add(vbox)
+        grid = Gtk.Grid(column_spacing = 50, row_spacing=7)
+        self.add(grid)
 
         self.progressbar = Gtk.ProgressBar()
-        vbox.pack_start(self.progressbar, True, True, 0)
+
+        start_button = Gtk.Button.new_with_label("Rest")
+        start_button.connect("clicked", \
+                self.start_button_clicked)
+
+        postpone_button = Gtk.Button.new_with_label("Postpone")
+        postpone_button.connect("clicked", \
+                self.postpone_button_clicked)
+        
+        grid.attach(self.progressbar, 0, 0, 2, 1)
+        grid.attach(postpone_button, 0, 1, 1, 1)
+        grid.attach(start_button, 1, 1, 1, 1)
+        
 
         self.timeout_id = GObject.timeout_add(50, self.on_timeout, resttime)
 
+    def start_button_clicked(self, button):
+        Gtk.main_quit() # for now
+    
+    def postpone_button_clicked(self, button):
+        print("Хуй")
+        #pop.destroy() # for now
+
     def on_timeout(self, resttime):
         """
-        Update value on the progress bar
+        update value on the progress bar
         """
-        new_value = self.progressbar.get_fraction() + fraction
-        print(new_value) # debug, remove later
+        value = self.progressbar.get_fraction() + fraction
+        # print(new_value) # debug, remove later
 
-        if new_value > 1:
-            new_value = 0
+        if time.time() == timeout or time.time() > timeout:
             Gtk.main_quit()
             pop.destroy()
+        
+        if value > 1:
+            value = 0
 
-        self.progressbar.set_fraction(new_value)
+        self.progressbar.set_fraction(value)
 
-        # As this is a timeout function, return True so that it
+        # as this is a timeout function, return True so that it
         # continues to get called
         return True
 
-#def main():
-#    if len(sys.argv) == 1:
-#        print("bse.py -w <mins to work> -r <mins to rest>")
-#        sys.exit(1)
-#    try:
-#        opts, args = getopt.getopt(sys.argv[1:],"w:r:", \
-#                ["work=","rest="])
-#    except getopt.GetoptError:
-#        print("bse.py -w <mins to work> -r <mins to rest>")
-#        sys.exit(2)
-#
-#    for opt, arg in opts:
-#        if opt in ("-w","--work"):
-#            worktime = float(arg)
-#        elif opt in ("-r","--rest"):
-#            breaktime = float(arg)
-#
-#    running = 1
-#    while(running):
-#        print(time.asctime())
-#        time.sleep(60*worktime)
-#        # popup shows up now
-#        print(time.asctime() + " Go get some rest now.")
-#        # time.sleep(60*breaktime)
-#        # aaaand it's gone
-#        print(time.asctime() + " Ok, now back to work.")
-
-print(time.asctime())
+print(time.asctime() + " Started.")
 running = True
 while(running):
     time.sleep(60*worktime)
     # popup shows up now
-    print(time.asctime() + " Go get some rest now.")
+    timeout = time.time() + 60*resttime
+    print(time.asctime() + " Rest timer starts now. ")
     pop = ReminderPopup()
-    pop.connect("delete-event", Gtk.main_quit)
+    pop.connect("delete-event", Gtk.main_quit) # delete when postpone button is working
     pop.show_all()
     Gtk.main()
-    # time.sleep(60*breaktime)
     # aaaand it's gone
-    print(time.asctime() + " Ok, now back to work.")
-# if __name__ == "__main__":
-#     main()
+    print(time.asctime() + " Work timer starts now.")
